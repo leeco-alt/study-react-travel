bf7d29757cac629a12a88fbb4837626e5fe01520
Leeco <leeco19@qq.com>
2022-09-10 16:08:11 +0800

    use middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,13 +2,14 @@ import { legacy_createStore as createStore, combineReducers, applyMiddleware } f
 import languageReducer from './language/languageReducer'
 import recommendProductsReducer from './recommendProducts/recommendProductsReducer'
 import thunk from 'redux-thunk'
+import { actionLog } from './middlewares/actionLog'
 
 const rootReducer = combineReducers({
   language: languageReducer,
   recommendProducts: recommendProductsReducer
 })
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk, actionLog))
 
 export type RootState = ReturnType<typeof store.getState>
 
