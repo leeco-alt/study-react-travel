7cbc4012783d5265590921710938a88d3c53c8de
Leeco <leeco19@qq.com>
2022-09-13 13:20:36 +0800

    Using Redux for shopping cart pages

--- src/layouts/mainLayout/MainLayout.tsx
+++ src/layouts/mainLayout/MainLayout.tsx
@@ -4,7 +4,7 @@ import { Header, Footer } from '../../components'
 
 interface PropsType {
   // 这里可能需要再优化
-  children?: JSX.Element[]
+  children?: JSX.Element[] | JSX.Element
 }
 
 export const MainLayout: React.FC<PropsType> = (props) => {
--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styles from './ShoppingCart.module.css'
+import { MainLayout } from '../../layouts/mainLayout'
+import { Row, Col, Affix } from 'antd'
+import { ProductList, PaymentCard } from '../../components'
 
-export const ShoppingCartPage: React.FC = () => {
+export const ShoppingCartPage: React.FC = (props) => {
   return (
-    <div>
-      <h1>ShoppingCart</h1>
-    </div>
+    <MainLayout>
+      <Row>
+        {/* 购物清单 */}
+        <Col span={16}></Col>
+        <div className={styles['product-list-container']}>
+          {/* <ProductList /> */}
+        </div>
+        {/* 支付卡组件 */}
+        <Col span={8}>
+          <div className={styles['payment-card-container']}>
+            {/* <PaymentCard /> */}
+          </div>
+        </Col>
+      </Row>
+    </MainLayout>
   )
 }
